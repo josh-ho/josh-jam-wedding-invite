bfd2464369f574f18d18bb9a721a5458b1ac10f3
Josh <josh.pi@gmail.com>
2017-01-03 11:18:57 -0500

    added some dev dependencies

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 8000;
 
+app.use( express.static( 'public' ) );
+
 // set the home page route
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(port, function() {
